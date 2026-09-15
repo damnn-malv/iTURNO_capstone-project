4ec4accb8993675dd8502b9f057c55680c1b47c9
Dan Malvin <23200010@gmail.com>
2026-09-15 16:43:18 +0800

    modified:   backend/api/models.py 	modified:   backend/db.sqlite3 	deleted:    backend/env

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,36 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import EmailValidator
 from django.db import models
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('The username (email) must be set')
+        user = self.model(username=self.normalize_email(username), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, password, **extra_fields)
+    
 class User(AbstractUser):
     email = None
     REQUIRED_FIELDS = []
@@ -14,6 +42,8 @@ class User(AbstractUser):
         help_text='Used as the account email address and login.',
         verbose_name='email address',
     )
+    
+    objects = UserManager()
 
     ROLE_CHOICES = [('PERSONNEL', 'Personnel'), ('SUPERVISOR', 'Supervisor'), ('MANAGER', 'Manager'), ('SUPERADMIN', 'Admin')]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='PERSONNEL')
